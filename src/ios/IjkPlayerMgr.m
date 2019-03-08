34cfa4184e5d5eef6662c54409f175e777f31216
linhao <linhao@ninthart.cn>
2019-03-08 16:38:02 +0800

    提交iOS

---
+++ src/ios/IjkPlayerMgr.m
@@ -0,0 +1,256 @@
+/*
+ * Apache 2.0 License
+ *
+ * Copyright (c) Hao Lin 2019
+ *
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Apache License
+ * Version 2.0 (the 'License'). You may not use this file except in
+ * compliance with the License. Please obtain a copy of the License at
+ * http://opensource.org/licenses/Apache-2.0/ and read it before using this
+ * file.
+ *
+ * The Original Code and all software distributed under the License are
+ * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+ * Please see the License for the specific language governing rights and
+ * limitations under the License.
+ */
+
+// codebeat:disable[TOO_MANY_FUNCTIONS]
+
+#import "IjkPlayerMgr.h"
+#import <IJKMediaFramework/IJKMediaFramework.h>
+
+@interface IjkPlayerMgr ()
+
+@property (readwrite, assign) BOOL deviceready;
+@property (readwrite, assign) BOOL isActive;
+@property (nonatomic, retain) IJKFFMoviePlayerController *ijkPlayer;
+
+- (IJKFFOptions *)optionsByDefault;
+
+@end
+
+@implementation IjkPlayerMgr
+
+@synthesize deviceready, ijkPlayer;
+
+#pragma mark -
+#pragma mark Interface
+
+- (void) playVideo:(CDVInvokedUrlCommand*)command{
+    NSString *videoUrl = command.arguments[0];
+
+    
+    UIWindow * window = [[UIApplication sharedApplication] keyWindow];
+    UIView *rootView = [[window subviews] objectAtIndex:0];
+    
+    UIView *displayView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, rootView.bounds.size.width, rootView.bounds.size.height)];
+    displayView.backgroundColor = [UIColor blackColor];
+    
+    self.ijkPlayer = [[IJKFFMoviePlayerController alloc] initWithContentURL:[NSURL URLWithString: videoUrl]
+                                                                withOptions:[self optionsByDefault]];
+    [self.ijkPlayer setScalingMode:IJKMPMovieScalingModeAspectFill];
+    UIView *playerView = [self.ijkPlayer view];
+    playerView.frame = displayView.bounds;
+    playerView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [displayView insertSubview:playerView atIndex:1];
+    [rootView insertSubview:displayView atIndex:2];
+    [self.ijkPlayer play];
+}
+
+- (void) removeVideo:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void) disconnectVideo:(CDVInvokedUrlCommand*)command{
+    
+}
+
+#pragma mark -
+#pragma mark Life Cycle
+
+/**
+ * Registers obervers after plugin was initialized.
+ */
+- (void) pluginInitialize
+{
+    NSLog(@"pluginInitialize");
+}
+
+#pragma mark -
+#pragma mark Helper
+
+
+/**
+ * Invokes the callback without any parameter.
+ *
+ * @return [ Void ]
+ */
+- (void) execCallback:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult *result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK];
+
+    [self.commandDelegate sendPluginResult:result
+                                callbackId:command.callbackId];
+}
+
+/**
+ * Invokes the callback with a single boolean parameter.
+ *
+ * @return [ Void ]
+ */
+- (void) execCallback:(CDVInvokedUrlCommand*)command arg:(BOOL)arg
+{
+    CDVPluginResult *result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsBool:arg];
+
+    [self.commandDelegate sendPluginResult:result
+                                callbackId:command.callbackId];
+}
+
+
+- (IJKFFOptions *)optionsByDefault
+{
+    IJKFFOptions *options = [[IJKFFOptions alloc] init];
+    
+    [options setPlayerOptionIntValue:30     forKey:@"max-fps"];
+    [options setPlayerOptionIntValue:1L      forKey:@"framedrop"];
+    [options setPlayerOptionIntValue:3      forKey:@"video-pictq-size"];
+    [options setPlayerOptionIntValue:0      forKey:@"videotoolbox"];
+    [options setPlayerOptionIntValue:960    forKey:@"videotoolbox-max-frame-width"];
+    
+    [options setFormatOptionIntValue:0                  forKey:@"auto_convert"];
+    [options setFormatOptionIntValue:1                  forKey:@"reconnect"];
+    [options setFormatOptionIntValue:30 * 1000 * 1000   forKey:@"timeout"];
+    [options setFormatOptionValue:@"ijkplayer"          forKey:@"user-agent"];
+    
+    [options setFormatOptionIntValue:0L        forKey:@"packet-buffering"];
+    [options setFormatOptionIntValue:100L      forKey:@"analyzemaxduration"];
+    [options setFormatOptionIntValue:10240L      forKey:@"probesize"];
+    [options setFormatOptionIntValue:1L      forKey:@"flush_packets"];
+    
+    options.showHudView   = NO;
+    
+    return options;
+}
+
+#pragma Selector func
+
+- (void)loadStateDidChange:(NSNotification*)notification {
+    IJKMPMovieLoadState loadState = ijkPlayer.loadState;
+    
+    if ((loadState & IJKMPMovieLoadStatePlaythroughOK) != 0) {
+        NSLog(@"LoadStateDidChange: IJKMovieLoadStatePlayThroughOK: %d\n",(int)loadState);
+    }else if ((loadState & IJKMPMovieLoadStateStalled) != 0) {
+        NSLog(@"loadStateDidChange: IJKMPMovieLoadStateStalled: %d\n", (int)loadState);
+    } else {
+        NSLog(@"loadStateDidChange: ???: %d\n", (int)loadState);
+    }
+}
+
+- (void)moviePlayBackFinish:(NSNotification*)notification {
+    int reason =[[[notification userInfo] valueForKey:IJKMPMoviePlayerPlaybackDidFinishReasonUserInfoKey] intValue];
+    switch (reason) {
+        case IJKMPMovieFinishReasonPlaybackEnded:
+            NSLog(@"playbackStateDidChange: IJKMPMovieFinishReasonPlaybackEnded: %d\n", reason);
+            break;
+            
+        case IJKMPMovieFinishReasonUserExited:
+            NSLog(@"playbackStateDidChange: IJKMPMovieFinishReasonUserExited: %d\n", reason);
+            break;
+            
+        case IJKMPMovieFinishReasonPlaybackError:
+            NSLog(@"playbackStateDidChange: IJKMPMovieFinishReasonPlaybackError: %d\n", reason);
+            break;
+            
+        default:
+            NSLog(@"playbackPlayBackDidFinish: ???: %d\n", reason);
+            break;
+    }
+}
+
+- (void)mediaIsPreparedToPlayDidChange:(NSNotification*)notification {
+    NSLog(@"mediaIsPrepareToPlayDidChange\n");
+}
+
+- (void)moviePlayBackStateDidChange:(NSNotification*)notification {
+    switch (ijkPlayer.playbackState) {
+        case IJKMPMoviePlaybackStateStopped:
+            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: stoped", (int)ijkPlayer.playbackState);
+            break;
+            
+        case IJKMPMoviePlaybackStatePlaying:
+            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: playing", (int)ijkPlayer.playbackState);
+            break;
+            
+        case IJKMPMoviePlaybackStatePaused:
+            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: paused", (int)ijkPlayer.playbackState);
+            break;
+            
+        case IJKMPMoviePlaybackStateInterrupted:
+            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: interrupted", (int)ijkPlayer.playbackState);
+            break;
+            
+        case IJKMPMoviePlaybackStateSeekingForward:
+        case IJKMPMoviePlaybackStateSeekingBackward: {
+            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: seeking", (int)ijkPlayer.playbackState);
+            break;
+        }
+            
+        default: {
+            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: unknown", (int)ijkPlayer.playbackState);
+            break;
+        }
+    }
+}
+
+#pragma Install Notifiacation
+
+- (void)installMovieNotificationObservers {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(loadStateDidChange:)
+                                                 name:IJKMPMoviePlayerLoadStateDidChangeNotification
+                                               object:ijkPlayer];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(moviePlayBackFinish:)
+                                                 name:IJKMPMoviePlayerPlaybackDidFinishNotification
+                                               object:ijkPlayer];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(mediaIsPreparedToPlayDidChange:)
+                                                 name:IJKMPMediaPlaybackIsPreparedToPlayDidChangeNotification
+                                               object:ijkPlayer];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(moviePlayBackStateDidChange:)
+                                                 name:IJKMPMoviePlayerPlaybackStateDidChangeNotification
+                                               object:ijkPlayer];
+    
+}
+
+- (void)removeMovieNotificationObservers {
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:IJKMPMoviePlayerLoadStateDidChangeNotification
+                                                  object:ijkPlayer];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:IJKMPMoviePlayerPlaybackDidFinishNotification
+                                                  object:ijkPlayer];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:IJKMPMediaPlaybackIsPreparedToPlayDidChangeNotification
+                                                  object:ijkPlayer];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:IJKMPMoviePlayerPlaybackStateDidChangeNotification
+                                                  object:ijkPlayer];
+    
+}
+
+
+@end
+
+// codebeat:enable[TOO_MANY_FUNCTIONS]
